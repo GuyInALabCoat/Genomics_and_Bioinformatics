d237c334ef8e50f43150898bbaf5b0f819559f62
Dmytro <dmytropolitov@gmail.com>
2016-11-04 17:23:55 -0400

    Add comments

--- transcription.pl
+++ transcription.pl
@@ -1,5 +1,5 @@
 $dna = 'GCAATGG';
 print "The DNA sequence is $dna\n";
 $rna = $dna;
-$rna =~ tr/T/U/;
+$rna =~ tr/T/U/;                          # use transliteration operator tr/// to replace all Ts with Us
 print "and the RNA sequence is $rna\n";
